a50130cad19f975b0cf56d461ecabf7c53b2a1ef
Lais Costa <lcosta.developer@gmail.com>
2023-03-05 10:16:15 -0300

    Criado view preguntar

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express()
 
 //setando o ejs como renderizador de html
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-app.get("/", function(req, res){
-    res.render("principal/perfil")
+app.get("/", (req, res) => {
+
+    res.render("index")
+})
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar")
 })
 
 
-app.listen(3000, () => { console.log("App rodando!")})
+app.listen(3000, () => { console.log("App rodando!") })
