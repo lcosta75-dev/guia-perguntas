7f94d407f2dde072ca5a5ea963540e658d2771b7
Lais Costa <lcosta.developer@gmail.com>
2023-03-07 18:45:02 -0300

    submetendo formulário

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 
 //setando o ejs como renderizador de html
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+//body parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//rotas
 app.get("/", (req, res) => {
 
     res.render("index")
@@ -12,6 +18,12 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar")
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    res.send("Formulário recebido! Titulo: " + titulo + descricao)
 })
 
 
