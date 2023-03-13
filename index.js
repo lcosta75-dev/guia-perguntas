8ccf6b7c3cbbac12c1f886b04ff95596b3631004
Lais Costa <lcosta.developer@gmail.com>
2023-03-13 19:21:05 -0300

    Concatenando e redirecionando para o ID da pergunta

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require("body-parser")
 const connection = require("./database/database")   //carregando a conexão
 const Pergunta = require("./database/Pergunta")
+const Resposta = require("./database/Resposta")
 
 //database
 connection
@@ -64,6 +65,17 @@ app.get("/pergunta/:id", (req, res) => {
         } else {
             res.redirect("/")
         }
+    })
+})
+
+app.post("/responder", (req, res) => {
+    var corpo = req.body.corpo
+    var perguntaId = req.body.pergunta
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/"+perguntaId)
     })
 })
 
